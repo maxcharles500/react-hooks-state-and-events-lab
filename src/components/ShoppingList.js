9c3a9145f6b3f7626671298f10680894121ef0b5
maximilianjones <maxcharles500@gmail.com>
2021-12-16 19:21:54 -0800

    commit tree 1eef4a4614b6b169226c83be48b1b5416264dd73

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,21 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [category, setCategory] = useState('All')
+
+  const handleCategory = (event) => {
+    // const filteredItems = items.filter(item => {
+    //   event.target.value === item.category
+    // })
+    setCategory(event.target.value)
+  }
+
+  const mappedItems = items => {
+    return items.map((item) => (
+      <Item key={item.id} name={item.name} category={item.category} />
+    ))
+  }
+
+  const handleFilter = category => {
+    if (category === 'All') {
+      return items
+    }
+    else {
+      return items.filter (item => {
+        return category === item.category
+      })
+
+    }
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
+        <select name="filter" onChange={handleCategory}>
+          <option value="All" >Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {mappedItems(handleFilter(category))} 
+        
       </ul>
     </div>
   );
